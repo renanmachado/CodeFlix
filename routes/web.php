7ec8f5afd842643b60e70019257cd7fbe8ecdecd
Renan Machado <renanmachadoloureiro@gmail.com>
2017-08-23 09:47:03 -0300

    VIDEOS: Init CRUD

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::group([
 		Route::resource('users', 'UsersController');
         Route::resource('categories', 'CategoriesController');
         Route::resource('series', 'SeriesController');
+        Route::resource('videos', 'VideosController');
 	});
 
 	Route::post('login', 'Auth\LoginController@login');
